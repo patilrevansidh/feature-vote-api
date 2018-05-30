9e456995bc5bbc5b2d77e97cd4425ad6857f15b9
Revansiddh Patil <rpatil1@DSRM.local>
2018-05-30 12:50:32 +0530

    api bug fixes

--- src/controlers/features/index.js
+++ src/controlers/features/index.js
@@ -18,15 +18,21 @@ router.get('/',(req,res)=>{
 
 
 router.post('/',(req,res)=>{    
-    connection.query(`INSERT INTO features(title, description,vote,voted_people) VALUES(?,?,?,?)`,[req.body.title,req.body.description,"0","vote:[]"], function (error, results, fields) {
+    const voted_people = {vote:[]}
+    connection.query(`INSERT INTO features(title, description,vote,voted_people) VALUES(?,?,?,?)`,[req.body.title,req.body.description,"0",JSON.stringify(voted_people)], function (error, results, fields) {
         if (error) {
-            console.log("error in row insert",error)
+            // console.log("error in row insert",error)
             throw error
             res.send(error)
         };
-        console.log("row inserted",results)
-        const response = helper.prepareSuccessBody(results);
-        res.json(response)
+        connection.query('SELECT * from features', function (error, results, fields) {
+            if (error) {
+                throw error
+                res.send(error)
+            };
+            const response = helper.prepareSuccessBody(results);
+            res.json(response)
+        });
     });
 })
 
@@ -48,16 +54,28 @@ router.put('/:featureId/vote',(req,res)=>{
         if(error) {
             console.log("error in casting vote",error)
         }
+        console.log("result",results[0])
         const record = JSON.parse(results[0].voted_people);
+        console.log("record",record)
         const voted_people = [...record.vote,userID]
+        console.log("voted_people",voted_people)
         const obj = JSON.stringify({vote:voted_people})
+        console.log("object",obj)
         const vote_length = voted_people.length;
         connection.query("UPDATE features SET voted_people = ?, vote = ? where id = ?",[obj, `${vote_length}` ,req.params.featureId],(error, results, fields)=>{
             if(error) {
                 console.log("error in casting vote",error)
             }
-       }) 
-       res.send("casted vote")
+            connection.query('SELECT * from features', function (error, results, fields) {
+                if (error) {
+                    throw error
+                    res.send(error)
+                };            
+                const response = helper.prepareSuccessBody(results);
+                console.log("cast vote ",response)
+                res.json(response)
+            });
+       })
     })
 })
 
