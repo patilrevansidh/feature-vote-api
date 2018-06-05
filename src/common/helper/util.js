e15a7fe8b4937a1df01027bd9a0ad38d6d6c3aed
Revansiddh Patil <rpatil1@DSRM.local>
2018-06-05 12:57:39 +0530

    authorization middle ware

--- src/common/helper/util.js
+++ src/common/helper/util.js
@@ -13,6 +13,16 @@ const stringObjToArray = function (obj,key) {
     return arrObj[key]
 }
 
+const isAuthorized = function (req, res, next) {
+    const user_id = req.get('user_id');
+    if(user_id) {
+        next();
+    }else {
+        res.send("Unauthorized User");
+    }
+}
+
 exports.prepareErrorBody = prepareErrorBody;
 exports.prepareSuccessBody = prepareSuccessBody;
 exports.stringObjToArray = stringObjToArray;
+exports.isAuthorized = isAuthorized
