f57a49037a22762099f98516adeaa5bf833fd647
Revansiddh Patil <rpatil1@DSRM.local>
2018-06-05 12:57:23 +0530

    feature functionality allowed to loggedin user

--- src/controlers/features/index.js
+++ src/controlers/features/index.js
@@ -5,27 +5,29 @@ const helper = require('../../common/helper/util');
 
 connection.connect();
 
-router.get('/',(req,res)=>{
-    connection.query('SELECT * from features', function (error, results, fields) {
+router.get('/',helper.isAuthorized,(req,res)=>{
+    connection.query('SELECT * from features',function (error, results, fields) {
         if (error) {
             throw error
             res.send(error)
         };
+        console.log("get all features",results)
         const response = helper.prepareSuccessBody(results);
         res.json(response)
     });
 })
 
 
-router.post('/',(req,res)=>{    
-    const voted_people = {vote:[]};
-    const invited = {invited:[]};
-    const comments_by = {comments:[]};
-    connection.query(`INSERT INTO features(title, description,vote,voted_people,invited,comments_by) VALUES(?,?,?,?,?,?)`,[req.body.title,req.body.description,"0",JSON.stringify(voted_people),JSON.stringify(invited),JSON.stringify(comments_by)], function (error, results, fields) {
+router.post('/',helper.isAuthorized,(req,res)=>{    
+    const voted_people = '';
+    const invited = req.body.invited != undefined ? req.body.invited : '' ;
+    const comments = '';
+    const created_by = req.header('user_id');
+    connection.query(`INSERT INTO features(title, description, vote, voted_people, invited, comments,created_by) VALUES
+                            (?,?,?,?,?,?,?)`,
+                            [req.body.title, req.body.description, "0", voted_people, invited, comments,created_by], function (error, results, fields) {
         if (error) {
-            // console.log("error in row insert",error)
             throw error
-            res.send(error)
         };
         connection.query('SELECT * from features', function (error, results, fields) {
             if (error) {
@@ -38,7 +40,7 @@ router.post('/',(req,res)=>{
     });
 })
 
-router.get('/:featureId',(req,res)=>{
+router.get('/:featureId',helper.isAuthorized,(req,res)=>{
     connection.query("SELECT * FROM features WHERE id= ?",[req.params.featureId], function (error, results, fields) {
         if (error) {
             console.log("error in row read",error)
@@ -56,17 +58,17 @@ router.get('/:featureId',(req,res)=>{
     });
 })
 
-router.put('/:featureId/vote',(req, res)=>{
+router.put('/:featureId/vote',helper.isAuthorized,(req, res)=>{
     const userID = req.body.user_id
     connection.query("SELECT * FROM features WHERE id= ?",[req.params.featureId],(error, results, fields)=>{
         if(error) {
             console.log("error in casting vote",error)
         }
-        const record = JSON.parse(results[0].voted_people);
-        const voted_people = [...record.vote,userID]
-        const obj = JSON.stringify({vote:voted_people})
-        const vote_length = voted_people.length;
-        connection.query("UPDATE features SET voted_people = ?, vote = ? where id = ?",[obj, `${vote_length}` ,req.params.featureId],(error, results, fields)=>{
+        const record = results[0];
+        const voted_people = record.voted_people.split(',').filter(d=>d);
+        const newVoted = [...voted_people,userID]
+        const vote_length = newVoted.length;
+        connection.query("UPDATE features SET voted_people = ?, vote = ? where id = ?",[newVoted.toString(), `${vote_length}` ,req.params.featureId],(error, results, fields)=>{
             if(error) {
                 console.log("error in casting vote",error)
             }
@@ -82,7 +84,7 @@ router.put('/:featureId/vote',(req, res)=>{
     })
 })
 
-router.post('/:featureId/comments',(req, res)=>{
+router.post('/:featureId/comments',helper.isAuthorized,(req, res)=>{
     const user_id = req.header('user_id');
     const feature_id = req.params.featureId;
     let newID;
@@ -105,7 +107,7 @@ router.post('/:featureId/comments',(req, res)=>{
 })
 
 
-router.delete('/:featureId',(req,res)=>{
+router.delete('/:featureId',helper.isAuthorized,(req,res)=>{
     connection.query("DELETE FROM features WHERE id= ?",[req.params.featureId], function (error, results, fields) {
         if (error) {
             console.log("error in row deletion",error)
