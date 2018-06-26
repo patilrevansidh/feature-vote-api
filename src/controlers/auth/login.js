05db0662c38cda0c524fb9221bbc3c592c6e3ac4
Revansiddh Patil <rpatil1@DSRM.local>
2018-06-26 12:03:21 +0530

    stored in encrypted form in db

--- src/controlers/auth/login.js
+++ src/controlers/auth/login.js
@@ -2,22 +2,33 @@ const router = require('express').Router()
 const connection = require('../../config/db');
 const helper = require('../../common/helper/util');
 var passport = require('passport');
-// var GoogleStrategy = require('passport-google-oauth').OAuthStrategy;
+const bcrypt = require('bcrypt');
 const GooglePlusTokenStrategy = require('passport-google-plus-token');
 
 
 router.post('/login',(req,res)=>{
-    console.log("request",req)
-    connection.query("SELECT * from users where username=? and password=?",[req.body.username,req.body.password],(error,result,fields)=>{
+
+    connection.query("SELECT * from users where username=?",[req.body.username],(error,result,fields)=>{
         if(error) {
             console.log("login error",error)
         }
-        if(Array.isArray(result) && result.length == 0) {
-            const user = helper.prepareErrorBody("Incorrect Username or Password")
-            res.send(user)
-        }else {
-            
-            const user = helper.prepareSuccessBody(result[0])
+        if(Array.isArray(result) && result.length > 0) {
+            let isMatch = undefined;
+            let user = {}
+            result.forEach((r)=>{
+                isMatch = bcrypt.compareSync(req.body.password,r.password)
+                if(isMatch) {
+                    delete r.password
+                    const user = helper.prepareSuccessBody(r)
+                    res.send(user);
+                }
+            });
+            if(!isMatch) {
+                const user = helper.prepareErrorBody('Invalid username or password')
+                res.send(user)
+            }
+        }else {            
+            const user = helper.prepareErrorBody('Invalid username or password')
             res.send(user)
         }
     })
